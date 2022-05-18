4f0095b3a1cf39031f7aa15be2014d4cb9822122
iamminji <minji.dev@gmail.com>
2022-05-18 22:55:26 +0900

    spring webflux 샘플 코드 추가

---
+++ java-spring-webflux/src/main/java/com/example/webflux/SampleController.java
@@ -0,0 +1,24 @@
+package com.example.webflux;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+@RestController
+@RequestMapping("/sample")
+@RequiredArgsConstructor
+public class SampleController {
+
+    @GetMapping(
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE
+    )
+    public Flux<Long> findWeather() {
+        return Flux.interval(Duration.ofSeconds(3))
+                .take(5);
+    }
+}
---
+++ java-spring-webflux/src/main/java/com/example/webflux/SecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.webflux;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain webHttpSecurity(ServerHttpSecurity http) {
+        http
+                .authorizeExchange((exchanges) -> exchanges
+                        .pathMatchers("/sample").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .formLogin()
+                .and()
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER")
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
+}
