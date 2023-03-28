34cf28c4a158c6638aa9d23844a697cbdd158436
iamminji <kimminji.dev@gmail.com>
2023-03-28 22:10:23 +0900

    [spring-event] spring event 예시 작성

--- java-spring-event/src/main/java/com/example/javaspringevent/controller/EmployeeController.java
+++ java-spring-event/src/main/java/com/example/javaspringevent/controller/EmployeeController.java
@@ -16,8 +16,6 @@ public class EmployeeController {
 
     @GetMapping("/notify/event")
     public void publishEvent() {
-        emailPublisher.publishEmailEvent(new EmailEvent("hello world"));
-        emailPublisher.publishEmailEvent(new EmailEvent("hello world success false", false));
         emailPublisher.publishEmailEvent(new EmailEvent("hello world true", true));
     }
 }
--- java-spring-event/src/main/java/com/example/javaspringevent/listener/ConditionEmailEventListener.java
+++ java-spring-event/src/main/java/com/example/javaspringevent/listener/ConditionEmailEventListener.java
@@ -3,6 +3,8 @@ package com.example.javaspringevent.listener;
 import com.example.javaspringevent.event.EmailEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 public class ConditionEmailEventListener {
@@ -12,4 +14,13 @@ public class ConditionEmailEventListener {
         System.out.println("(success) email event 3 :" + event);
     }
 
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+    public void handleTransactionalDoBeforeCommit(EmailEvent event) {
+        System.out.println("email event before commit:" + event);
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    public void handleTransactionalDoAfterCommit(EmailEvent event) {
+        System.out.println("email event after commit:" + event);
+    }
 }
